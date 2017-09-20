63206898aab1efb18de555bbb306bf61d54ea198
harmoniemand <jonathan.guenz@outlook.com>
2017-09-20 03:49:58 +0200

    frontend

--- frontend/src/controllers/map.controller.js
+++ frontend/src/controllers/map.controller.js
@@ -2,39 +2,38 @@
 //var L = require('leaflet');
 
 module.exports = ['$scope', '$http', function (scope, $http) {
-
     $http({
         method: 'GET',
-        url: 'http'
+        url: 'https://dthack17.de/api/sensors'
     }).then(function successCallback(response) {
-        // this callback will be called asynchronously
-        // when the response is available
+
+        var data = [];
+        response.data.forEach(function(item) {
+            if (!(item.device && item.latitude && item.longitude && item.acceleration))
+                return;
+            
+            data.push({
+                device: item.device,
+                lat: item.latitude,
+                long: item.longitude,
+                acc: item.acceleration
+            });
+        });
+
+        var map = L.map('map').setView([50.7067745, 7.1289442], 13);
+
+        L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
+
+        data.forEach(function (item) {
+            L.marker([item.lat, item.long])
+                .addTo(map)
+                .bindPopup('ACC: ' + item.acc + " - DEVICE: " + item.device)
+                .openPopup();
+        });
     }, function errorCallback(response) {
         // called asynchronously if an error occurs
         // or server returns response with an error status.
     });
-
-    var data = [
-        {
-            lat: 50.7067745,
-            long: 7.1289442,
-            acc: 1.7,
-            device: 'android'
-        }
-    ];
-
-
-    var map = L.map('map').setView([50.7067745, 7.1289442], 13);
-
-    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
-
-    data.forEach(function (item) {
-        L.marker([item.lat, item.long])
-            .addTo(map)
-            .bindPopup('ACC: ' + item.acc + " - DEVICE: " + item.device)
-            .openPopup();
-    })
-
 }];
